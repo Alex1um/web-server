512b4825861a2acb3205d6d33baf83f33144a6b9
Alex1um <assassin20563@gmail.com>
2023-07-12 14:33:00 +0700

    first commit

---
+++ web-server.ino
@@ -0,0 +1,221 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+#define CAMERA_MODEL_AI_THINKER
+//#define CAMERA_MODEL_M5STACK_PSRAM
+//#define CAMERA_MODEL_M5STACK_WITHOUT_PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM_B
+//#define CAMERA_MODEL_WROVER_KIT
+
+#if defined(CAMERA_MODEL_WROVER_KIT)
+  #define PWDN_GPIO_NUM    -1
+  #define RESET_GPIO_NUM   -1
+  #define XCLK_GPIO_NUM    21
+  #define SIOD_GPIO_NUM    26
+  #define SIOC_GPIO_NUM    27
+  
+  #define Y9_GPIO_NUM      35
+  #define Y8_GPIO_NUM      34
+  #define Y7_GPIO_NUM      39
+  #define Y6_GPIO_NUM      36
+  #define Y5_GPIO_NUM      19
+  #define Y4_GPIO_NUM      18
+  #define Y3_GPIO_NUM       5
+  #define Y2_GPIO_NUM       4
+  #define VSYNC_GPIO_NUM   25
+  #define HREF_GPIO_NUM    23
+  #define PCLK_GPIO_NUM    22
+
+#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
+  #define PWDN_GPIO_NUM     -1
+  #define RESET_GPIO_NUM    15
+  #define XCLK_GPIO_NUM     27
+  #define SIOD_GPIO_NUM     25
+  #define SIOC_GPIO_NUM     23
+  
+  #define Y9_GPIO_NUM       19
+  #define Y8_GPIO_NUM       36
+  #define Y7_GPIO_NUM       18
+  #define Y6_GPIO_NUM       39
+  #define Y5_GPIO_NUM        5
+  #define Y4_GPIO_NUM       34
+  #define Y3_GPIO_NUM       35
+  #define Y2_GPIO_NUM       32
+  #define VSYNC_GPIO_NUM    22
+  #define HREF_GPIO_NUM     26
+  #define PCLK_GPIO_NUM     21
+
+#elif defined(CAMERA_MODEL_M5STACK_WITHOUT_PSRAM)
+  #define PWDN_GPIO_NUM     -1
+  #define RESET_GPIO_NUM    15
+  #define XCLK_GPIO_NUM     27
+  #define SIOD_GPIO_NUM     25
+  #define SIOC_GPIO_NUM     23
+  
+  #define Y9_GPIO_NUM       19
+  #define Y8_GPIO_NUM       36
+  #define Y7_GPIO_NUM       18
+  #define Y6_GPIO_NUM       39
+  #define Y5_GPIO_NUM        5
+  #define Y4_GPIO_NUM       34
+  #define Y3_GPIO_NUM       35
+  #define Y2_GPIO_NUM       17
+  #define VSYNC_GPIO_NUM    22
+  #define HREF_GPIO_NUM     26
+  #define PCLK_GPIO_NUM     21
+
+#elif defined(CAMERA_MODEL_AI_THINKER)
+  #define PWDN_GPIO_NUM     32
+  #define RESET_GPIO_NUM    -1
+  #define XCLK_GPIO_NUM      0
+  #define SIOD_GPIO_NUM     26
+  #define SIOC_GPIO_NUM     27
+  
+  #define Y9_GPIO_NUM       35
+  #define Y8_GPIO_NUM       34
+  #define Y7_GPIO_NUM       39
+  #define Y6_GPIO_NUM       36
+  #define Y5_GPIO_NUM       21
+  #define Y4_GPIO_NUM       19
+  #define Y3_GPIO_NUM       18
+  #define Y2_GPIO_NUM        5
+  #define VSYNC_GPIO_NUM    25
+  #define HREF_GPIO_NUM     23
+  #define PCLK_GPIO_NUM     22
+
+#elif defined(CAMERA_MODEL_M5STACK_PSRAM_B)
+  #define PWDN_GPIO_NUM     -1
+  #define RESET_GPIO_NUM    15
+  #define XCLK_GPIO_NUM     27
+  #define SIOD_GPIO_NUM     22
+  #define SIOC_GPIO_NUM     23
+  
+  #define Y9_GPIO_NUM       19
+  #define Y8_GPIO_NUM       36
+  #define Y7_GPIO_NUM       18
+  #define Y6_GPIO_NUM       39
+  #define Y5_GPIO_NUM        5
+  #define Y4_GPIO_NUM       34
+  #define Y3_GPIO_NUM       35
+  #define Y2_GPIO_NUM       32
+  #define VSYNC_GPIO_NUM    25
+  #define HREF_GPIO_NUM     26
+  #define PCLK_GPIO_NUM     21
+
+#else
+  #error "Camera model not selected"
+#endif
+
+#define MOTOR_1_PIN_1    14
+#define MOTOR_1_PIN_2    15
+#define MOTOR_2_PIN_1    13
+#define MOTOR_2_PIN_2    12
+
+WebServer server(80);
+
+const char *ssid = "Hackspace";
+const char *pass = "youathackspace";
+// const char *ssid = "just another spot";
+// const char *pass = "11111111";
+
+void go_forward() {
+  digitalWrite(MOTOR_1_PIN_1, 1);
+  digitalWrite(MOTOR_1_PIN_2, 0);
+  digitalWrite(MOTOR_2_PIN_1, 1);
+  digitalWrite(MOTOR_2_PIN_2, 0);
+}
+
+void go_left() {
+  digitalWrite(MOTOR_1_PIN_1, 0);
+  digitalWrite(MOTOR_1_PIN_2, 1);
+  digitalWrite(MOTOR_2_PIN_1, 1);
+  digitalWrite(MOTOR_2_PIN_2, 0);
+}
+
+void go_right() {
+  digitalWrite(MOTOR_1_PIN_1, 1);
+  digitalWrite(MOTOR_1_PIN_2, 0);
+  digitalWrite(MOTOR_2_PIN_1, 0);
+  digitalWrite(MOTOR_2_PIN_2, 1);
+}
+
+void go_backward() {
+  digitalWrite(MOTOR_1_PIN_1, 0);
+  digitalWrite(MOTOR_1_PIN_2, 1);
+  digitalWrite(MOTOR_2_PIN_1, 0);
+  digitalWrite(MOTOR_2_PIN_2, 1);
+}
+
+void stop() {
+  digitalWrite(MOTOR_1_PIN_1, 0);
+  digitalWrite(MOTOR_1_PIN_2, 0);
+  digitalWrite(MOTOR_2_PIN_1, 0);
+  digitalWrite(MOTOR_2_PIN_2, 0);
+}
+
+void go(String dir) {
+  if (dir.equals("forward")) {
+    go_forward();
+  } else if (dir.equals("backward")) {
+    go_backward();
+  } else if (dir.equals("left")) {
+    go_left();
+  } else if (dir.equals("right")) {
+    go_right();
+  } else {
+    stop();
+  }
+}
+
+void action() {
+  String direction;
+  String uri = server.uri();
+  String dest = uri.substring(uri.lastIndexOf("/") + 1);
+  Serial.println(server.uri());
+  Serial.println(server.args());
+  bool is_delayed = server.argName(0).equals("delay") && !dest.equals("stop");
+  go(dest);
+  if (is_delayed) {
+    delay(server.arg(0).toInt());
+    stop();
+  }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(MOTOR_1_PIN_1, OUTPUT);
+    pinMode(MOTOR_1_PIN_2, OUTPUT);
+    pinMode(MOTOR_2_PIN_1, OUTPUT);
+    pinMode(MOTOR_2_PIN_2, OUTPUT);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/go/forward", action);
+    server.on("/go/backward", action);
+    server.on("/go/left", action);
+    server.on("/go/right", action);
+    server.on("/go/stop", action);
+    server.begin();
+    Serial.println("HTTP server started");    
+
+}
+
+void loop()
+{
+  server.handleClient();
+  delay(2);//allow the cpu to switch to other tasks
+}
