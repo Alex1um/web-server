b29e6a75951b1e6639248acda6178ed535bdcb09
Alex1um <assassin20563@gmail.com>
2023-07-14 11:34:25 +0700

    acceleration sensor

--- web-server.ino
+++ web-server.ino
@@ -1,116 +1,29 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <Wire.h>
+#include <MPU9250_asukiaaa.h>
+// #include <CircularBuffer.h>
 
-#define CAMERA_MODEL_AI_THINKER
-//#define CAMERA_MODEL_M5STACK_PSRAM
-//#define CAMERA_MODEL_M5STACK_WITHOUT_PSRAM
-//#define CAMERA_MODEL_M5STACK_PSRAM_B
-//#define CAMERA_MODEL_WROVER_KIT
-
-#if defined(CAMERA_MODEL_WROVER_KIT)
-  #define PWDN_GPIO_NUM    -1
-  #define RESET_GPIO_NUM   -1
-  #define XCLK_GPIO_NUM    21
-  #define SIOD_GPIO_NUM    26
-  #define SIOC_GPIO_NUM    27
-  
-  #define Y9_GPIO_NUM      35
-  #define Y8_GPIO_NUM      34
-  #define Y7_GPIO_NUM      39
-  #define Y6_GPIO_NUM      36
-  #define Y5_GPIO_NUM      19
-  #define Y4_GPIO_NUM      18
-  #define Y3_GPIO_NUM       5
-  #define Y2_GPIO_NUM       4
-  #define VSYNC_GPIO_NUM   25
-  #define HREF_GPIO_NUM    23
-  #define PCLK_GPIO_NUM    22
-
-#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
-  #define PWDN_GPIO_NUM     -1
-  #define RESET_GPIO_NUM    15
-  #define XCLK_GPIO_NUM     27
-  #define SIOD_GPIO_NUM     25
-  #define SIOC_GPIO_NUM     23
-  
-  #define Y9_GPIO_NUM       19
-  #define Y8_GPIO_NUM       36
-  #define Y7_GPIO_NUM       18
-  #define Y6_GPIO_NUM       39
-  #define Y5_GPIO_NUM        5
-  #define Y4_GPIO_NUM       34
-  #define Y3_GPIO_NUM       35
-  #define Y2_GPIO_NUM       32
-  #define VSYNC_GPIO_NUM    22
-  #define HREF_GPIO_NUM     26
-  #define PCLK_GPIO_NUM     21
-
-#elif defined(CAMERA_MODEL_M5STACK_WITHOUT_PSRAM)
-  #define PWDN_GPIO_NUM     -1
-  #define RESET_GPIO_NUM    15
-  #define XCLK_GPIO_NUM     27
-  #define SIOD_GPIO_NUM     25
-  #define SIOC_GPIO_NUM     23
-  
-  #define Y9_GPIO_NUM       19
-  #define Y8_GPIO_NUM       36
-  #define Y7_GPIO_NUM       18
-  #define Y6_GPIO_NUM       39
-  #define Y5_GPIO_NUM        5
-  #define Y4_GPIO_NUM       34
-  #define Y3_GPIO_NUM       35
-  #define Y2_GPIO_NUM       17
-  #define VSYNC_GPIO_NUM    22
-  #define HREF_GPIO_NUM     26
-  #define PCLK_GPIO_NUM     21
-
-#elif defined(CAMERA_MODEL_AI_THINKER)
-  #define PWDN_GPIO_NUM     32
-  #define RESET_GPIO_NUM    -1
-  #define XCLK_GPIO_NUM      0
-  #define SIOD_GPIO_NUM     26
-  #define SIOC_GPIO_NUM     27
-  
-  #define Y9_GPIO_NUM       35
-  #define Y8_GPIO_NUM       34
-  #define Y7_GPIO_NUM       39
-  #define Y6_GPIO_NUM       36
-  #define Y5_GPIO_NUM       21
-  #define Y4_GPIO_NUM       19
-  #define Y3_GPIO_NUM       18
-  #define Y2_GPIO_NUM        5
-  #define VSYNC_GPIO_NUM    25
-  #define HREF_GPIO_NUM     23
-  #define PCLK_GPIO_NUM     22
-
-#elif defined(CAMERA_MODEL_M5STACK_PSRAM_B)
-  #define PWDN_GPIO_NUM     -1
-  #define RESET_GPIO_NUM    15
-  #define XCLK_GPIO_NUM     27
-  #define SIOD_GPIO_NUM     22
-  #define SIOC_GPIO_NUM     23
-  
-  #define Y9_GPIO_NUM       19
-  #define Y8_GPIO_NUM       36
-  #define Y7_GPIO_NUM       18
-  #define Y6_GPIO_NUM       39
-  #define Y5_GPIO_NUM        5
-  #define Y4_GPIO_NUM       34
-  #define Y3_GPIO_NUM       35
-  #define Y2_GPIO_NUM       32
-  #define VSYNC_GPIO_NUM    25
-  #define HREF_GPIO_NUM     26
-  #define PCLK_GPIO_NUM     21
-
-#else
-  #error "Camera model not selected"
+#define ESP_MODEL_WROOM
+#if defined(CAMERA_MODEL_AI_THINKER)
+  #define MOTOR_1_PIN_1    14
+  #define MOTOR_1_PIN_2    15
+  #define MOTOR_2_PIN_1    13
+  #define MOTOR_2_PIN_2    12
+  #define LED_PIN 33
+  #define LED_HIGH LOW
+  #define LED_LOW HIGH
+#elif defined(ESP_MODEL_WROOM)
+  #define MOTOR_1_PIN_1    32
+  #define MOTOR_1_PIN_2    33
+  #define MOTOR_2_PIN_1    25
+  #define MOTOR_2_PIN_2    26
+  #define LED_PIN 2
+  #define LED_HIGH HIGH
+  #define LED_LOW LOW
+  #define SDA_PIN 21
+  #define SCL_PIN 22
 #endif
-
-#define MOTOR_1_PIN_1    14
-#define MOTOR_1_PIN_2    15
-#define MOTOR_2_PIN_1    13
-#define MOTOR_2_PIN_2    12
-#define LED_PIN 33
 
 WebServer server(80);
 
@@ -118,6 +31,8 @@ const char *ssid = "Hackspace";
 const char *pass = "youathackspace";
 // const char *ssid = "just another spot";
 // const char *pass = "11111111";
+
+MPU9250_asukiaaa Sensor;
 
 void go_forward() {
   digitalWrite(MOTOR_1_PIN_1, 1);
@@ -169,12 +84,50 @@ void go(String dir) {
 }
 
 void led_on() {
-  digitalWrite(LED_PIN, LOW);
+  digitalWrite(LED_PIN, LED_HIGH);
 }
 
-
 void led_off() {
-  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(LED_PIN, LED_LOW);
+}
+
+// struct AccelData {
+//   float x;
+//   float y;
+//   float z;
+//   float sqrt;
+// };
+
+// CircularBuffer<AccelData, 10> accelBuf;
+
+// uint32_t listen(uint32_t time) {
+//   unsigned long start = millis();
+//   accelBuf.clear();
+//   while (millis() - start < time) {
+//     Sensor.accelUpdate();
+//     accelBuf.push(
+//       AccelData{ 
+//         .x=Sensor.accelX(),
+//         .y=Sensor.accelY(),
+//         .z=Sensor.accelZ(),
+//         .sqrt=Sensor.accelSqrt(),
+//       }
+//     );
+
+//   }
+//   return 0;
+// }
+
+float filtVal = 0.;
+uint32_t listen(uint32_t time) {
+  unsigned long start = millis();
+  while (millis() - start < time) {
+    Sensor.accelUpdate();
+    float newVal = (pow(Sensor.accelY(), 2) - filtVal) * .1;
+    if (newVal > .3) return 400;
+    filtVal += newVal;
+  }
+  return 200;
 }
 
 void action() {
@@ -186,9 +139,49 @@ void action() {
   bool is_delayed = server.argName(0).equals("delay") && !dest.equals("stop");
   go(dest);
   if (is_delayed) {
-    delay(server.arg(0).toInt());
+    int code = 200;
+    if (dest == "left" || dest == "right") {
+      delay(server.arg(0).toInt());
+    } else {
+      code = listen(server.arg(0).toInt());
+    }
     stop();
+    server.send(code);
   }
+}
+
+void get_gyro() {
+  Sensor.gyroUpdate();
+  char tmp[200];
+  sprintf(tmp, "\
+  {\
+    \"x\": %f,\
+    \"y\": %f,\
+    \"z\": %f\
+  }\
+  ",
+  Sensor.gyroX(), 
+  Sensor.gyroY(),
+  Sensor.gyroZ());
+  server.send(200, "application/json", tmp);
+}
+
+void get_accel() {
+  Sensor.accelUpdate();
+  char tmp[200];
+  sprintf(tmp, "\
+  {\
+    \"x\": %f,\
+    \"y\": %f,\
+    \"z\": %f,\
+    \"sqrt\": %f\
+  }\
+  ",
+  Sensor.accelX(), 
+  Sensor.accelY(), 
+  Sensor.accelZ(), 
+  Sensor.accelSqrt());
+  server.send(200, "application/json", tmp);
 }
 
 void setup()
@@ -199,6 +192,12 @@ void setup()
     pinMode(MOTOR_1_PIN_2, OUTPUT);
     pinMode(MOTOR_2_PIN_1, OUTPUT);
     pinMode(MOTOR_2_PIN_2, OUTPUT);
+    pinMode(LED_PIN, OUTPUT);
+    Wire.begin(SDA_PIN, SCL_PIN);
+    Sensor.setWire(&Wire);
+
+    Sensor.beginGyro();
+    Sensor.beginAccel();
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, pass);
@@ -221,12 +220,21 @@ void setup()
     server.on("/go/stop", action);
     server.on("/led/on", led_on);
     server.on("/led/off", led_off);
+    server.on("/info/accel", get_accel);
+    server.on("/info/gyro", get_gyro);
+    server.enableCORS();
     server.begin();
-    Serial.println("HTTP server started");    
-    pinMode(LED_PIN, OUTPUT);
+    Serial.println("HTTP server started");
+
 }
 
 void loop()
+{
+  server.handleClient();
+  delay(2);//allow the cpu to switch to other tasks
+}
+
+void yield()
 {
   server.handleClient();
   delay(2);//allow the cpu to switch to other tasks
